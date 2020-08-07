9aafa6fe415c01c05498578d0130f76b3824780e
nayeemfrontdev <nayeem.cmedhealth@gmail.com>
2020-08-07 13:34:58 +0600

    double== vs tripple===

---
+++ doubelvstripple.js
@@ -0,0 +1,15 @@
+const vhai = 1;
+const bon = true;
+
+if(vhai === bon){
+    console.log("true");
+}else{
+    console.log("false");
+}
+
+
+if(vhai == bon){
+    console.log("true");
+}else{
+    console.log("false");
+}
